12e131036cbf1d4768b8a6f2195555a801888aa4
Milo Wyner <milowyner@gmail.com>
2021-07-03 20:57:10 -0700

    Complete custom implementation of the app before reading the "showing the player's score with an alert" section

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alertIsShowing = false
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var guess: Int!
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                .foregroundColor(.white)
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        alertIsShowing = true
+                        guess = number
+                    }, label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    })
+                    .alert(isPresented: $alertIsShowing, content: {
+                        Alert(title: Text("\(guess == correctAnswer ? "Correct" : "Incorrect")!"), message: Text("You guessed \(countries[guess])!"), dismissButton: .default(Text("Play again"), action: {
+                            countries.shuffle()
+                            correctAnswer = Int.random(in: 0...2)
+                        }))
+                    })
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
