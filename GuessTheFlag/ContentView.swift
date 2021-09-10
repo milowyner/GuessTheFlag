b517b0b54d2844377b2c5df6ffb1c81cd510004e
Milo Wyner <milowyner@gmail.com>
2021-09-09 17:20:41 -0700

    Improvement from Project 15 on Accessibility: add labels for each flag image so people who use voice over can also play the game. Also I decided to use the combine children modifier to make the two text sections easier to read

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,6 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var wrongAnswer: Int?
@@ -66,12 +80,14 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .fontWeight(.bold)
                 }
+                .accessibilityElement(children: .combine)
                 
                 ForEach(0..<3) { number in
                     Button(action: {
                         flagTapped(number)
                     }, label: {
                         FlagImage(country: countries[number])
+                            .accessibility(label: Text(labels[countries[number], default: "Unknown flag"]))
                             .rotation3DEffect(
                                 .degrees(number == correctAnswer ? flagRotation : 0),
                                 axis: (x: 0, y: 1, z: 0)
@@ -86,6 +102,7 @@ struct ContentView: View {
                     Text("\(score)")
                         .fontWeight(.bold)
                 }
+                .accessibilityElement(children: .combine)
                 
                 Spacer()
             }
--- GuessTheFlag/FlagImage.swift
+++ GuessTheFlag/FlagImage.swift
@@ -10,12 +10,19 @@ import SwiftUI
 struct FlagImage: View {
     let country: String
     
+    @State private var rotation = 0.0
+    
+    func rotate() {
+        
+    }
+    
     var body: some View {
         Image(country)
             .renderingMode(.original)
             .clipShape(Capsule())
             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
             .shadow(color: .black, radius: 2)
+            .rotation3DEffect(.degrees(rotation), axis: (x: 0, y: 1, z: 0))
     }
 }
 
